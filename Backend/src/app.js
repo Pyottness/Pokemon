43e89697a97af55ae6c96fd6f433b412b5ec7ab3
Pyottness <mark.j.pyott@gmail.com>
2022-02-07 15:44:42 +0100

    backend tweak

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -16,7 +16,8 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
-//pokemon Gen1
+
+//pokemon First Generation
 
 router.get('/api/gen1', (req, res, next) => {
   const gen1 = [
